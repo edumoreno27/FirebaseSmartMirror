348d48c173928c7a28b4f115d0a4089789e71249
zaratec31@hotmail.com <u201314894@upc.edu.pe>
2019-04-16 18:18:33 -0500

    Arreglo de firebase

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ const host = 'https://smartmirror-api.azurewebsites.net';
 const functions = require('firebase-functions');
 const app = dialogflow({ debug: true });
 
-var usuarioID = undefined;
+var usuarioID = "";
 
 
 function callApiUsuario() {
@@ -38,7 +38,7 @@ app.intent('Diary', (conv, params) => {
     var orden = params.number;
     var orden2 = orden - 1;
     console.log(orden2, usuarioID);
-    usuarioID = usuarioID.replace('\'', '');
+    // usuarioID = usuarioID.replace('\'', '');
     console.log(orden2, usuarioID);
     return callUpdateDiaries(orden2, usuarioID).then(data => {
         return conv.ask(`Mostrando agenda ${orden}`);
@@ -51,7 +51,7 @@ app.intent('Diary', (conv, params) => {
 app.intent('CloseDiary', (conv) => {
 
 
-    usuarioID = usuarioID.replace('\'', '');
+    // usuarioID = usuarioID.replace('\'', '');
     console.log(usuarioID);
     return callApiOcultarAgenda(usuarioID).then(data => {
         return conv.ask(`Cerrando agenda`);
@@ -61,7 +61,7 @@ app.intent('CloseDiary', (conv) => {
 
 
 function callUpdateDiaries(orden, usuarioid) {
-    let data = JSON.stringify({ order: orden, userId: usuarioid });
+    let data = JSON.stringify({ 'order': orden, 'userId': usuarioid });
     console.log(data);
     return new Promise((resolve, reject) => {
         const options = {
@@ -72,7 +72,7 @@ function callUpdateDiaries(orden, usuarioid) {
         };
 
         request(options, function(error, requestInternal, body) {
-            resolve(JSON.parse(body));
+            resolve(body);
         });
     });
 }
@@ -89,7 +89,7 @@ function callApiOcultarAgenda(usuarioid) {
         };
 
         request(options, function(error, requestInternal, body) {
-            resolve(JSON.parse(body));
+            resolve(body);
         });
     });
 }
